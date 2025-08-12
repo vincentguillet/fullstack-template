55715fb38d67d5839f8fa9880d4aa8a42323a46b
Vincent Guillet <vincent.guillet@outlook.com>
2025-08-12 16:56:15 +0200

    Enhance navbar with user authentication features and improve layout

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatButton} from '@angular/material/button';
 import {RouterLink} from '@angular/router';
+import {AuthService} from '../../services/auth/auth.service';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
@@ -9,11 +12,16 @@ import {RouterLink} from '@angular/router';
   imports: [
     MatToolbar,
     MatButton,
-    RouterLink
+    RouterLink,
+    MatMenuTrigger,
+    MatIcon,
+    MatMenu,
+    MatMenuItem
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  protected readonly authService = inject(AuthService);
 }
