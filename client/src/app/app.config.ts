1df31562bd13fd2670ebf3a65521dd0ec455f8f9
Vincent Guillet <vincent.guillet@outlook.com>
2025-08-12 16:56:33 +0200

    Add APP_INITIALIZER to bootstrap user session on app initialization

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,10 +1,12 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig, inject, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {authTokenInterceptor} from './interceptors/authToken/auth-token.interceptor';
+import {AuthService} from './services/auth/auth.service';
+import {catchError, firstValueFrom, of} from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +16,17 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([
         authTokenInterceptor
       ])
-    )
+    ),
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: () => {
+        const auth = inject(AuthService);
+        return () => firstValueFrom(auth.bootstrapSession().pipe(
+            catchError(err => of(null))
+          )
+        );
+      }
+    }
   ]
 };
