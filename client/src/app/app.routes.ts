acf36a7536e692f4a6dde68d07baccb0e55a28fe
Vincent Guillet <vincent.guillet@outlook.com>
2025-08-13 09:06:00 +0200

    Add routing for profile and admin components with appropriate guards

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {RegisterComponent} from './pages/register/register.component';
 import {LoginComponent} from './pages/login/login.component';
+import {ProfileComponent} from './pages/profile/profile.component';
+import {guestOnlyCanActivate, guestOnlyCanMatch} from './guards/guest-only/guest-only.guard';
+import {authOnlyCanMatch} from './guards/auth-only/auth-only.guard';
+import {AdminComponent} from './pages/admin/admin.component';
+import {adminOnlyCanMatch} from './guards/admin-only/admin-only.guard';
 
 export const routes: Routes = [
   {
@@ -19,11 +24,28 @@ export const routes: Routes = [
   },
   {
     path: 'register',
-    component: RegisterComponent
+    component: RegisterComponent,
+    canMatch: [guestOnlyCanMatch],
+    canActivate: [guestOnlyCanActivate]
   },
   {
     path : 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    canMatch: [guestOnlyCanMatch],
+    canActivate: [guestOnlyCanActivate]
+
+  },
+  {
+    path : 'profile',
+    component: ProfileComponent,
+    canMatch: [authOnlyCanMatch],
+    canActivate: [authOnlyCanMatch]
+  },
+  {
+    path : 'admin',
+    component: AdminComponent,
+    canMatch: [adminOnlyCanMatch],
+    canActivate: [adminOnlyCanMatch]
   },
   {
     path : '**',
